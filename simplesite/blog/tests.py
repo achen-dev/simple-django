c9a1bb392dc0f4aeaf71383749ed5a4c34a53939
achen-dev <chenhaojunchj@gmail.com>
2023-03-06 21:03:08 +1100

    Made easier test

--- simplesite/blog/tests.py
+++ simplesite/blog/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class APIPlaygroundViewTest(TestCase):
+    def testdummytest(self):
+        variable_gen = 5
+        self.assertEquals(variable_gen,5)
