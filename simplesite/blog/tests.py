db524d7e8849a9acefd583f2b235ed440ec34145
achen-dev <chenhaojunchj@gmail.com>
2023-03-06 20:25:06 +1100

    First test call

--- simplesite/blog/tests.py
+++ simplesite/blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class APIPlaygroundViewTests(TestCase):
+    def test_numbers_api(self):
+        api_call = "23"
+        url = reverse('blog:apiPlayground', )
+        response = self.client.get(url)
+        self.assertContains(response.status_code, 200)
