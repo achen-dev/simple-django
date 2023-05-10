2395ee2068d0c2ee1bf8dae033bdde7706403253
achen-dev <chenhaojunchj@gmail.com>
2023-05-10 17:03:02 +1000

    Working connect 4 board and piece placement

--- simplesite/blog/AICode.py
+++ simplesite/blog/AICode.py
@@ -2,16 +2,8 @@
 This code drives the backend logic and AI agent for the connect 4 game
 """
 
-# Setup the game board in a flat rotation where the left is the bottom and the right is the top
-GAME_STATE = [
-    ["X", "X", "X", "X", "X", "X"],
-    ["X", "X", "X", "X", "X", "X"],
-    ["X", "X", "X", "X", "X", "X"],
-    ["X", "X", "X", "X", "X", "X"],
-    ["X", "X", "X", "X", "X", "X"],
-    ["X", "X", "X", "X", "X", "X"],
-    ["X", "X", "X", "X", "X", "X"],
-]
+# imports
+import itertools
 
 # Helper code for generating board
 # for i in range(7):
@@ -22,23 +14,62 @@ GAME_STATE = [
 #     print(str)
 
 
-def print_game_state():
+def initialise_game():
+    """
+    Set up the game board in a flat rotation where the left is the bottom and the right is the top
+    """
+    return [
+        ["X", "X", "X", "X", "X", "X"],
+        ["X", "X", "X", "X", "X", "X"],
+        ["X", "X", "X", "X", "X", "X"],
+        ["X", "X", "X", "X", "X", "X"],
+        ["X", "X", "X", "X", "X", "X"],
+        ["X", "X", "X", "X", "X", "X"],
+        ["X", "X", "X", "X", "X", "X"],
+        ]
+
+
+def print_game_state(gamestate):
     """
     Prints the current game state
     """
     for i in range(5, -1, -1):
         str = ""
         for j in range(7):
-            str += GAME_STATE[j][i] + " "
+            str += gamestate[j][i] + " "
         print(str)
-
+    print("_____________")
+    print("0 1 2 3 4 5 6")
 def game_end(gamestate):
     """
-    Checks if the game has ended
+    Checks if the game has ended, placeholder for now
     """
     return False
 
 
+def place_piece(gamestate, player, column):
+    for slot in range(len(gamestate[column])):
+        if gamestate[column][slot] == "X":
+            gamestate[column][slot] = player
+            return gamestate, (column, slot)
+    return "Invalid move"
+
 
 if __name__ == "__main__":
-    print_game_state()
+    players = itertools.cycle(["A", "B"])
+    game_state = initialise_game()
+    print_game_state(game_state)
+    while True:
+        if game_end(game_state):
+            break
+        player = next(players)
+        print("Player " + player + "'s turn")
+        column = int(input("Column: "))
+        next_state, last_piece = place_piece(game_state, player, column)
+        if next_state == "Invalid move":
+            print("Invalid move")
+            continue
+        else:
+            game_state = next_state
+        print("_____________")
+        print_game_state(game_state)
